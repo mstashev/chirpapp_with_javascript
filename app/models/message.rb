c6f91439f4998d7cc451cbf0de9e18fe07b5bfb6
Michael Stashevsky <mstashev@gmail.com>
2017-03-05 21:58:52 -0500

    Fixed error in Timeline. Wish Barry Allen would stop running off into the future.

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,7 +5,7 @@ class Message < ApplicationRecord
   validates_format_of :body, with: /\A.{1,160}\z/, on: :create
 
   def self.timeline(user)
-    following_ids = user.followees.pluck(:id)
+    following_ids = user.followees(User).pluck(:id)
     all_ids= following_ids << user.id
     Message.where(user_id: all_ids).order("created_at DESC")
   end
