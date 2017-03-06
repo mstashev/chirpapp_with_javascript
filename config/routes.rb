1dcc8f5ffd733add3f9417d5b28e537f668865ef
Michael Stashevsky <mstashev@gmail.com>
2017-03-05 19:54:47 -0500

    Fixed error in routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   post '/login'           => 'sessions#create'
   post '/signup'          => 'users#create'
   post '/timeline'        => 'messages#timeline'
-  post '/follow/:user_id' => 'user#follow'
+  post '/follow/:user_id' => 'users#follow'
   get  '/user_list'       => 'users#index'
   post '/messages_index'  => 'users#show'
   post '/message'         => 'messages#create' #=> Post a tweet
