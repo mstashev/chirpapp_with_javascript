99f5de859a970578a26957b6899ee70832900c05
Michael Stashevsky <mstashev@gmail.com>
2017-03-05 20:13:10 -0500

    Fixed following capabilities

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,9 +26,9 @@ class UsersController < ApplicationController
       # binding.pry
       current_user.toggle_follow!(User.find(params[:user_id]))
       if current_user.follows?(User.find_by(id: params[:user_id]))
-        render json: ["#{current_user.username} is now following #{User.find_by(id: params[:user_id]).username_email}."]
+        render json: ["#{current_user.name} is now following #{User.find_by(id: params[:user_id]).name}."]
       else
-        render json: ["#{current_user.username} has unfollowed #{User.find_by(id: params[:user_id]).username_email}."]
+        render json: ["#{current_user.name} has unfollowed #{User.find_by(id: params[:user_id]).name}."]
       end
     else
       require_user
